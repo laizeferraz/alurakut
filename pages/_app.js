40eaccce0d64818db1733138a8d24e02f1099799
Laize Ferraz <laizeferraz@gmail.com>
2021-07-14 10:44:31 +1000

    grid structure created for home.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,30 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+
+  body {
+    font-family: sans-serif;
+    background-color: #d9e6f6 ;
+  }
+
+  #_next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +34,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+  background: #fff;
+  border-radius: 0.5em;
+`;
+
+const MainGrid = styled.main`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 31.25em;
+  grid-gap: 0.6em;
+  padding: 1em;
+
+  .profileArea {
+    display: none;
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media (min-width: 850px) {
+    max-width: 69.4em;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationshipsArea";
+    grid-template-columns: 10em 1fr 19.5em;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea">
+        <Box>Profile</Box>
+      </div>
+      <div className="welcomeArea">
+        <Box>Welcome</Box>
+      </div>
+      <div className="profileRelationshipsArea">
+        <Box>Friends</Box>
+        <Box>Communities</Box>
+      </div>
+    </MainGrid>
+  );
 }
