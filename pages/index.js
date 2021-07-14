6ee1699182246b88a2ca6adc8d062f8d9eb8c3bd
Laize Ferraz <laizeferraz@gmail.com>
2021-07-14 15:11:07 +1000

    added a lib with all styles needed.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,66 @@
 import { MainGrid } from "../src/components/MainGrid/index";
 import { Box } from "../src/components/Box/index";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelationships";
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        alt="profile"
+        style={{ borderRadius: "0.5em" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
+  const githubUser = "laizeferraz";
+  const friends = [
+    "juunegreiros",
+    "omariosouto",
+    "peas",
+    "rafaballerini",
+    "marcobrunodev",
+    "felipefialho",
+  ];
   return (
-    <MainGrid>
-      <div className="profileArea">
-        <Box>
-          <img src="https://github.com/laizeferraz.png" alt="profile" />
-        </Box>
-      </div>
-      <div className="welcomeArea">
-        <Box>Welcome</Box>
-      </div>
-      <div className="profileRelationshipsArea">
-        <Box>Friends</Box>
-        <Box>Communities</Box>
-      </div>
-    </MainGrid>
+    <div>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea">
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea">
+          <Box>
+            <h1 className="title">Welcome</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationshipsArea">
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Friends ({friends.length})</h2>
+
+            <ul>
+              {friends.map((friend) => {
+                return (
+                  <li>
+                    <a href={`/users/${friend}`} key={friend}>
+                      <img src={`https://github.com/${friend}.png`} />
+                      <span>{friend}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>Communities</Box>
+        </div>
+      </MainGrid>
+    </div>
   );
 }
